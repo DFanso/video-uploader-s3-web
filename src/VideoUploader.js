0e75e8bb6e3ded995ff7e73f34509dedab9388db
Leo Gavin <leogavin123@outlook.com>
2023-08-28 11:35:55 +0530

    add new features and bug fix

--- src/VideoUploader.js
+++ src/VideoUploader.js
@@ -6,75 +6,99 @@ import { TailSpin } from 'react-loader-spinner';
 const VideoUploader = () => {
   const [uploadedUrls, setUploadedUrls] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [selectedVideos, setSelectedVideos] = useState([]);
+  const [videoQueue, setVideoQueue] = useState([]);
+  const [errorAlert, setErrorAlert] = useState(null);
 
-  const onDrop = async (acceptedFiles) => {
-    const formData = new FormData();
-    acceptedFiles.forEach((file) => {
-      formData.append('videos', file);
-    });
-    setSelectedVideos([...selectedVideos, ...acceptedFiles]);
-  }
-  
-  const removeVideo = (file) => {
-    const updatedSelectedVideos = selectedVideos.filter((video) => video !== file);
-    setSelectedVideos(updatedSelectedVideos);
+  const onDrop = (acceptedFiles) => {
+    setVideoQueue([...videoQueue, ...acceptedFiles]);
   };
-  
-  const handleUpload = async () => {
-    setIsLoading(true);
 
-    const formData = new FormData();
-    selectedVideos.forEach((video) => {
-      formData.append('videos', video);
-    });
-    try {
-        const response = await axios.post('http://45.76.155.253:3001/upload', formData, {
+  const removeVideoFromQueue = (index) => {
+    const updatedQueue = [...videoQueue];
+    updatedQueue.splice(index, 1);
+    setVideoQueue(updatedQueue);
+  };
+
+  const handleUploadQueue = async () => {
+    setIsLoading(true);
+    setErrorAlert(null);
+
+    setUploadedUrls([]);
+  
+    const updatedQueue = [...videoQueue];
+    const uploadedUrlsArray = [];
+    const failedVideos = []; // Store names of failed videos
+  
+    while (updatedQueue.length > 0) {
+      const currentVideo = updatedQueue.shift();
+      const formData = new FormData();
+      formData.append('video', currentVideo);
+  
+      try {
+        const response = await axios.post('http://localhost:3001/upload', formData, {
           headers: {
             'Content-Type': 'multipart/form-data',
           },
         });
   
-        setUploadedUrls(response.data.uploadedUrls);
+        uploadedUrlsArray.push(response.data.uploadedUrl);
       } catch (error) {
-        console.error('Error uploading videos:', error);
-      } finally {
-          setIsLoading(false);
-        }
+        console.error(`Error uploading video ${currentVideo.name}:`, error);
+        failedVideos.push(currentVideo.name); // Add failed video name to the array
+      }
+    }
+  
+    setUploadedUrls([...uploadedUrls, ...uploadedUrlsArray]);
+    setIsLoading(false);
+    setVideoQueue([]);
+  
+    if (failedVideos.length > 0) {
+      setErrorAlert(`Failed to upload the following video(s): ${failedVideos.join(', ')}`);
+    }
+  };
 
-    
-    };
-
-    return (
-        <div className="main-content">
-          <h2>Upload Videos</h2>
-          <Dropzone onDrop={onDrop}>
-            {({ getRootProps, getInputProps }) => (
-              <div {...getRootProps()} className="dropzone">
-                <input {...getInputProps()} />
-                <p>Drag and drop video files here, or click to select files</p>
-              </div>
-            )}
-          </Dropzone>
-          {selectedVideos.length > 0 && (
-            <div className="selected-videos">
-              <h3>Selected Videos:</h3>
-              <ul>
-                {selectedVideos.map((video, index) => (
-                  <li key={index}>
-                    {video.name} <button onClick={() => removeVideo(video)}>Remove</button>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-          <button className="upload-button" onClick={handleUpload} disabled={isLoading || selectedVideos.length === 0}>
+  return (
+    <div className="main-content">
+      <h2>Upload Videos</h2>
+      <Dropzone onDrop={onDrop} accept="video/*" multiple>
+        {({ getRootProps, getInputProps }) => (
+          <div {...getRootProps()} className="dropzone">
+            <input {...getInputProps()} />
+            <p>Drag and drop video files here, or click to select files</p>
+          </div>
+        )}
+      </Dropzone>
+      {videoQueue.length > 0 && (
+        <div className="selected-videos">
+          <h3>Selected Videos:</h3>
+          <ul>
+            {videoQueue.map((video, index) => (
+              <li key={index}>
+                {video.name}{' '}
+                <button className="remove-button" onClick={() => removeVideoFromQueue(index)}>
+                  Remove
+                </button>
+              </li>
+            ))}
+          </ul>
+          <button
+            className="upload-button custom-upload-button"
+            onClick={handleUploadQueue}
+            disabled={isLoading || videoQueue.length === 0}
+          >
             Upload Videos
           </button>
+        </div>
+      )}
       {isLoading && (
         <div className="loader">
           <TailSpin color="#00BFFF" height={40} width={40} />
           <p>Uploading videos... Please wait.</p>
+        </div>
+      )}
+      {errorAlert && (
+        <div className="error-alert">
+          {errorAlert}
         </div>
       )}
       {uploadedUrls.length > 0 && (
